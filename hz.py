97ce56dd750f9aac9ffe7f22703f94105b44f3b0
Panda <xamnez@gmail.com>
2025-07-05 00:40:35 +0300

    sliders

--- hz.py
+++ hz.py
@@ -1,12 +1,11 @@
 import flet as ft
 import flet_audio as fa
 import os
+import time
 import random
 import yt_dlp
-
-current_song = None
-current_vol=0.2
-
+import threading
+    
 #изменение громкости с прогресбарорм
 #прогресбар трека
 #фуфелшмерц краш
@@ -20,16 +19,33 @@ current_vol=0.2
 if not os.path.isdir('songs'):os.makedirs('songs')
 def main(page: ft.Page):
     page.title = "СПОТИФАЕИНАТОР 3000"
+    current_song = None
+    current_vol=20
+    is_playing=False
+    dur_bar=ft.Slider(min=0, max=1, height=10,on_change_end=lambda e:dur_slider_change_end(e) if current_song else None)
+    song_title=ft.Text(size=25,text_align=ft.MainAxisAlignment.CENTER,value='No active song')
+
 
     #page.vertical_alignment=ft.MainAxisAlignment.CENTER
-
     #FUNC \/
+    def passive_update_dur_slider():
+        nonlocal current_song,is_playing
+        while True:
+            if current_song:
+                print(1000*int(is_playing))
+                time.sleep(1)
+                dur_bar.value+=1000*int(is_playing)
+                page.update()
+
+    def dur_slider_change_end(e):
+        current_song.seek(int(e.control.value))
 
     def hide_volume_bar(e):
         volume_slider.visible=False
         page.update()
+
     def change_vol(e):
-        global current_vol
+        nonlocal current_vol
         current_vol=e.control.value/100
         current_song.volume=current_vol
         page.update()
@@ -54,43 +70,50 @@ def main(page: ft.Page):
         page.update()
         update_playlist_display(songs)
         
-
     def next_song(playlist):
-        global current_song
+        nonlocal current_song
         current_song.pause()
         num=current_song.data
         if num+1<len(playlist):
             play_song(playlist[num+1])
             
         else:
-            current_song=playlist[0]
-            current_song.play()
+           play_song(playlist[0])
 
     def play_random(playlist):
-        global current_song
+        nonlocal current_song
         if current_song:current_song.pause()
         #shuffle(playlist=playlist)
-        current_song=playlist[random.randint(0,len(playlist)-1)]
-        current_song.play()
+        play_song(playlist[random.randint(0,len(playlist)-1)])
 
     def play_song(song):
-        global current_song
+        nonlocal current_song,dur_bar,song_title,is_playing
         if current_song in page.overlay: current_song.pause()
         current_song=song
         if current_song not in page.overlay:return
         if current_song: current_song.pause()
+        is_playing=True
         current_song.play()
-        page.bottom_appbar.content.value='Now playing :'+current_song.src.split('/')[-1][:-4]
-        page.update()
+        song_title.value='Now playing: '+current_song.src.split('/')[-1][:-4]
+        
+        dur_bar.max=song.get_duration()
+        dur_bar.value=0
+        page.bottom_appbar.content=ft.Column([dur_bar,song_title])
+        #create_dur_bar(song)
+        #page.update()
 
     def stop(e):
-        global current_song
-        if current_song:current_song.pause()
+        nonlocal current_song,is_playing
+        if current_song:
+            current_song.pause()
+            is_playing=False
 
     def resume(e):
-        global current_song
-        if current_song: current_song.resume()
-
+        nonlocal current_song,is_playing
+        if current_song: 
+            current_song.resume()
+            is_playing=True
+    
     def shuffle(playlist=None):
         random.shuffle(playlist)
         res=[song for song in playlist]
@@ -100,8 +123,6 @@ def main(page: ft.Page):
         songs=res
         update_playlist_display(playlist)
         page.update()
-        print(songs)
-    
     
     def update_playlist_display(playlist):
         lv.controls.clear()
@@ -117,6 +138,8 @@ def main(page: ft.Page):
     def on_download_complete(d):
         if d['status']=='finished':
             update_songs()
+    
+    threading.Thread(target=passive_update_dur_slider, daemon=True).start()
     #FUNC   /\
     #GUI    \/
     yt_options={
@@ -127,13 +150,13 @@ def main(page: ft.Page):
     ydl = yt_dlp.YoutubeDL(yt_options)
 
     volume_slider=ft.Slider(
-        min=0,max=100,value=current_vol,visible=False,
-        on_change=change_vol,
-        on_change_end=hide_volume_bar
-    )
-
+        min=0,max=100,value=current_vol,
+        on_change=change_vol
+        #on_change_end=hide_volume_bar
+        )
+    
     page.bottom_appbar=ft.BottomAppBar(
-        content=ft.Text(size=25,text_align=ft.MainAxisAlignment.CENTER,value='No active song'),                        
+        content=ft.Column([dur_bar, song_title]),                        
     )
 
     dlg_confirm=ft.AlertDialog(
@@ -158,7 +181,7 @@ def main(page: ft.Page):
     songs=[fa.Audio(src=f'l:/pr/mp3/songs/{song}',autoplay=False,volume=current_vol,on_state_changed=state_change,data=i) for i,song in enumerate(os.listdir('songs'))]
     for song in songs:page.overlay.append(song)
 
-    lv=ft.ListView(width=400,height=400)
+    lv=ft.ListView(width=400,height=400,expand=True)
     for song in songs:
         lv.controls.append(ft.ListTile(title=ft.Text(song.src.split('/')[-1][:-4]),
                                        on_click=lambda e,t=song:play_song(t),
@@ -169,7 +192,7 @@ def main(page: ft.Page):
 
 
     page.add(
-        ft.Row(
+        ft.Row(#верхние кнопки
             [
                 ft.IconButton(icon=ft.Icons.PAUSE,on_click=stop),
                 ft.ElevatedButton(text='Play random',on_click=lambda _:play_random(songs)),
@@ -179,7 +202,7 @@ def main(page: ft.Page):
                 ],
         alignment=ft.MainAxisAlignment.CENTER),
 
-        ft.Column([
+        ft.Column([ #меню скачки
                     link_field,
                     download_button
                     ],
@@ -188,23 +211,21 @@ def main(page: ft.Page):
         ft.Row(
                 [ft.Column(
                     [
-                        ft.Row([ft.Container(content=lv),
+                        ft.Row([ft.Container(content=lv),   #плейлист и его обновление
                                 ft.IconButton(ft.Icons.REPEAT,on_click=update_songs)
-                    ]),
+                                ],expand=True),
                         ft.Row(
-                            [
-                                ft.IconButton(icon=ft.Icons.VOLUME_UP,on_hover=show_volume_bar),
+                            [   ft.IconButton(icon=ft.Icons.VOLUME_UP),#громкость
                                 volume_slider
-                            ],spacing=0
+                            ],spacing=0,expand=True
                             )
                          ],
                             alignment=ft.MainAxisAlignment.START,
                             spacing=0,expand=True
-                            ),
+                            )
 
-                    ]
+                    ],expand=True
                 )
-
-)
+    )
 
 ft.app(main,view=ft.AppView.FLET_APP_WEB)
