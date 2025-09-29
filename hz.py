d2e6627c147e5d6c0365a483cfde649b34bc8884
umpra <xamnez@gmail.com>
2025-09-29 13:53:22 +0300

    help

--- hz.py
+++ hz.py
@@ -5,18 +5,22 @@ import time
 import random
 import yt_dlp
 import threading
-    
-#изменение громкости с прогресбарорм
-#прогресбар трека
+from youtube_api import YouTubeDataAPI
+
+
+
 #фуфелшмерц краш
 #удаление через gu
-#отображение текущего трека
-#загрузка через исполнитель+песня
+
+#плейлисты
+# загрузка через исполнитель+песня
 #интеграция плейлистов спотифая
 #редактирование файла через gu
 #псхалка на тройное нажатие обновления
 
+yt = YouTubeDataAPI('AIzaSyC-ZQhsOfHUFz1HjxjuBunPGZ8e8dnHaWk')
 if not os.path.isdir('songs'):os.makedirs('songs')
+lock = threading.Lock()
 def main(page: ft.Page):
     page.title = "СПОТИФАЕИНАТОР 3000"
     current_song = None
@@ -27,22 +31,21 @@ def main(page: ft.Page):
 
 
     #page.vertical_alignment=ft.MainAxisAlignment.CENTER
-    #FUNC \/
+    #FUNC \/    
     def passive_update_dur_slider():
         nonlocal current_song,is_playing
         while True:
             if current_song:
-                print(1000*int(is_playing))
                 time.sleep(1)
-                dur_bar.value+=1000*int(is_playing)
-                page.update()
+                if 1000*int(is_playing)+dur_bar.value<=dur_bar.max:
+                    dur_bar.value+=1000*int(is_playing)
+                    page.update()
 
     def dur_slider_change_end(e):
         current_song.seek(int(e.control.value))
 
     def hide_volume_bar(e):
         volume_slider.visible=False
-        page.update()
 
     def change_vol(e):
         nonlocal current_vol
@@ -52,22 +55,26 @@ def main(page: ft.Page):
 
     def show_volume_bar(e):
         volume_slider.visible=True
-        page.update()
 
     def state_change(e):
         if e.data=='completed' and e.control in page.overlay: next_song(songs)
     
-    def download_song(link=None):
-        if not link:link=link_field.value
-        if '&list' not in link:ydl.download(link)
-        else: page.open(dlg_confirm)
+    def download_song(url=None, artist=None,song_name=None):
+        if url:
+            if '&list' not in url:ydl.download(url)
+            else: page.open(dlg_playlist_confirm)
+        if song_name:
+            search_results=yt.search(q=f'{artist} {song_name}', max_results=1)[0]['video_id']
+            ydl.download(search_results)
+
+    def download_button():
+        pass
 
     def update_songs(e=None):
         nonlocal songs
         page.overlay.clear()
         songs=[fa.Audio(src=f'l:/pr/mp3/songs/{song}',autoplay=False,volume=current_vol,on_state_changed=state_change,data=i) for i,song in enumerate(os.listdir('songs'))]
         for song in songs:page.overlay.append(song)
-        page.update()
         update_playlist_display(songs)
         
     def next_song(playlist):
@@ -75,8 +82,7 @@ def main(page: ft.Page):
         current_song.pause()
         num=current_song.data
         if num+1<len(playlist):
-            play_song(playlist[num+1])
-            
+            play_song(playlist[num+1])  
         else:
            play_song(playlist[0])
 
@@ -93,6 +99,7 @@ def main(page: ft.Page):
         if current_song not in page.overlay:return
         if current_song: current_song.pause()
         is_playing=True
+        current_song.volume = current_vol
         current_song.play()
         song_title.value='Now playing: '+current_song.src.split('/')[-1][:-4]
         
@@ -122,8 +129,7 @@ def main(page: ft.Page):
         nonlocal songs
         songs=res
         update_playlist_display(playlist)
-        page.update()
-    
+
     def update_playlist_display(playlist):
         lv.controls.clear()
         nonlocal songs
@@ -132,53 +138,74 @@ def main(page: ft.Page):
                                        on_click=lambda e,t=song:play_song(t),
                                        title_alignment=ft.MainAxisAlignment.CENTER,
                                        ))
-        
         page.update()
 
     def on_download_complete(d):
         if d['status']=='finished':
             update_songs()
     
-    threading.Thread(target=passive_update_dur_slider, daemon=True).start()
+    page.run_thread(passive_update_dur_slider)
     #FUNC   /\
     #GUI    \/
     yt_options={
-        'format':'bestaudio',
+        'format':'best audio',
         'outtmpl':'songs/%(title)s.mp3',
-        'progress_hooks': [on_download_complete]
+        'progress_hooks': [on_download_complete],
         }
     ydl = yt_dlp.YoutubeDL(yt_options)
 
     volume_slider=ft.Slider(
         min=0,max=100,value=current_vol,
         on_change=change_vol
-        #on_change_end=hide_volume_bar
         )
     
     page.bottom_appbar=ft.BottomAppBar(
         content=ft.Column([dur_bar, song_title]),                        
     )
 
-    dlg_confirm=ft.AlertDialog(
+    dlg_playlist_confirm=ft.AlertDialog(
         title=ft.Text('Are you sure?'),
         content=ft.Text('This a PLAYLIST not a song'),
         actions=[
-            ft.ElevatedButton(text='Yes',on_click=lambda _:(page.close(dlg_confirm),
+            ft.ElevatedButton(text='Yes',on_click=lambda _:(page.close(dlg_playlist_confirm),
                                                             ydl.download(link_field.value)
                                                             )),
             ft.ElevatedButton(text='Download only song',on_click=lambda _:
-                                                    (page.close(dlg_confirm),
+                                                    (page.close(dlg_playlist_confirm),
                                                      ydl.download(link_field.value.split('&')[0]))),
-            ft.ElevatedButton(text='Cancel',on_click=lambda _: page.close(dlg_confirm))
+            ft.ElevatedButton(text='Cancel',on_click=lambda _: page.close(dlg_playlist_confirm))
         ]   
     )
 
+    link_field=ft.TextField(label='youtube link')
+    artist_field=ft.TextField(label='artist')
+    name_field=ft.TextField(label='name')
 
+    download_from_url_button=ft.ElevatedButton(text='Download from url',on_click=lambda _:download_song(url=link_field.value))
+    download_search_button=ft.ElevatedButton(text='Download with search',on_click=lambda _:download_song(
+    artist=artist_field.value,song_name=name_field.value))
+    download_song_button=ft.ElevatedButton(text='Download',on_click=lambda _:page.open(dlg_download))
 
-    link_field=ft.TextField(label='youtube link',width=400)
-    download_button=ft.ElevatedButton(text='Downlaod',on_click=lambda _:download_song())
-
-    songs=[fa.Audio(src=f'l:/pr/mp3/songs/{song}',autoplay=False,volume=current_vol,on_state_changed=state_change,data=i) for i,song in enumerate(os.listdir('songs'))]
+    dlg_download=ft.AlertDialog(
+        modal=True,
+        actions=[
+            ft.Container(
+                    ft.Row([
+                        ft.Column([
+                            link_field,
+                            download_from_url_button
+                        ]),
+                        ft.Column([
+                            artist_field,
+                            name_field,
+                            download_search_button
+                        ])
+                    ],width=600,vertical_alignment=ft.CrossAxisAlignment.CENTER),
+                    padding=25
+                )
+        ]
+    )
+    songs=[fa.Audio(src=f'l:/pr/mp3/songs/{song}',autoplay=False,volume=current_vol/100,on_state_changed=state_change,data=i) for i,song in enumerate(os.listdir('songs'))]
     for song in songs:page.overlay.append(song)
 
     lv=ft.ListView(width=400,height=400,expand=True)
@@ -190,10 +217,9 @@ def main(page: ft.Page):
         
     shuffle_button=ft.IconButton(icon=ft.Icons.CHAIR,on_click=lambda _:shuffle(playlist=songs),data=[i for i in songs])
 
-
     page.add(
         ft.Row(#верхние кнопки
-            [
+            [   
                 ft.IconButton(icon=ft.Icons.PAUSE,on_click=stop),
                 ft.ElevatedButton(text='Play random',on_click=lambda _:play_random(songs)),
                 ft.IconButton(icon=ft.Icons.ARROW_RIGHT,on_click=resume),
@@ -203,8 +229,8 @@ def main(page: ft.Page):
         alignment=ft.MainAxisAlignment.CENTER),
 
         ft.Column([ #меню скачки
-                    link_field,
-                    download_button
+                    #link_field,
+                    download_song_button
                     ],
         alignment=ft.MainAxisAlignment.END),
 
@@ -227,5 +253,4 @@ def main(page: ft.Page):
                     ],expand=True
                 )
     )
-
 ft.app(main,view=ft.AppView.FLET_APP_WEB)
